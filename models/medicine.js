d948d3e336a690bd27810f0678c29405f97364c2
Alaa Kayyali <akayyali.oe@gmail.com>
2025-11-20 09:35:54 +0300

    fix medicine model

--- models/medicine.js
+++ models/medicine.js
@@ -3,15 +3,12 @@ import sequelize from "../config/db.js";
 import { ulid } from "ulid";
 
 
+
 const Medicine = sequelize.define(
   "Medicine",
   {
-    id: {
-        type: DataTypes.STRING(26),
-        primaryKey: true,
-        defaultValue: () => ulid()
-    },
-   med_name: DataTypes.STRING,
+    id: { type: DataTypes.STRING, primaryKey: true, defaultValue: () => ulid() },
+    med_name: DataTypes.STRING,
     closet: DataTypes.STRING,
     category: DataTypes.STRING,
     description: DataTypes.TEXT,
@@ -20,11 +17,9 @@ const Medicine = sequelize.define(
     price: { type: DataTypes.DECIMAL(10, 2), defaultValue: 0.0 },
   },
   {
-    tableName: "medicine",     // ✅ match Laravel table
-    freezeTableName: true,      // ✅ don't pluralize
-    timestamps: true,           // ✅ use Laravel timestamps
-    createdAt: "created_at",    // ✅ match Laravel's column names
-    updatedAt: "updated_at",
+    tableName: "medicines",     
+    freezeTableName: true,    
+    timestamps: true
   }
 );
 
